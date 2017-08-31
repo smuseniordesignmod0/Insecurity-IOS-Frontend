e39ba066b33f336863a570a67be823afa8879f72
Gabo <gleyvamerino@smu.edu>
2017-08-31 00:06:15 -0500

    basic UI Implemented

--- Insecurity/DeviceReportViewController.swift
+++ Insecurity/DeviceReportViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ReportViewController.swift
+//  DeviceReportViewController.swift
 //  Insecurity
 //
 //  Created by Gabriel I Leyva Merino on 8/30/17.
@@ -11,22 +11,20 @@ import UIKit
 import MaterialComponents
 
 
-class ReportViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    @IBOutlet weak var networkNameLabel: UILabel!
-    @IBOutlet weak var firewallLabel: UILabel!
-    @IBOutlet weak var wirelessLabel: UILabel!
-    @IBOutlet weak var scoreLabel: UILabel!
+class DeviceReportViewController: UIViewController, UITableViewDelegate, UITableViewDataSource   {
+    
+    @IBOutlet weak var osLabel: UILabel!
+    @IBOutlet weak var ipLabel: UILabel!
+    @IBOutlet weak var macLabel: UILabel!
+    @IBOutlet weak var servicesLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
-    //fake data
-    let section = ["Devices", "Open Ports"]
-    let items = [["Device 1", "Device 2", "Device 3"], ["Port 1", "Port 2", "Port 3"]]
+    let section = ["Open Ports"]
+    let items = [["Port 1", "Port 2", "Port 3"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareTableView()
-       
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,7 +40,7 @@ class ReportViewController: UIViewController, UITableViewDelegate, UITableViewDa
     // MARK: - Table view data source and delegate
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-         return self.section[section]
+        return self.section[section]
     }
     
     func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
@@ -66,35 +64,14 @@ class ReportViewController: UIViewController, UITableViewDelegate, UITableViewDa
         cell.selectionStyle = .none
         
         cell.textLabel?.text = items[indexPath.section][indexPath.row]
-        
-        if indexPath.section == 0 {
-            //add more icon on the right side
-        }
-        
+                
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        tableView.deselectRow(at: indexPath, animated: true)
-        tableView.separatorStyle = .none
-        tableView.separatorStyle = .singleLine
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cells", for: indexPath as IndexPath)
-        cell.textLabel?.text = items[indexPath.section][indexPath.row]
-        cell.selectionStyle = .none
-        
-        if indexPath.section == 0 {
-            performSegue(withIdentifier: "viewDevice", sender: cell)
-            print("Device Selected")
-        }
-    }
-    
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         view.tintColor = .inBlue
         let header = view as! UITableViewHeaderFooterView
         header.textLabel?.textColor = .white
     }
-    
 
 }
